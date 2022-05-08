fc102b86040e3ac0cff06617584f5e84fe68422a
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-05-08 09:45:29 -0400

    done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-function QuestionItem({ question, onDeleteClick }) {
+function QuestionItem({ question, onDeleteClick, onUpdateQ }) {
   const { id, prompt, answers, correctIndex } = question;
-
+  // console.log(answers)
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
@@ -15,16 +15,20 @@ function handleDeleteClick(){
   })
     .then((r) => r.json())
     .then(() => onDeleteClick(question));
-
 }
 
+function handleChange(event){
+  event.preventDefault()
+  console.log(event.target.value)
+  onUpdateQ(id, parseInt(event.target.value))
+}
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleChange} defaultValue={correctIndex}>{options}</select>
       </label>
       <button onClick={handleDeleteClick} >Delete Question</button>
     </li>
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -15,19 +15,45 @@ fetch("http://localhost:4000/questions")
 const updatedQuestionList = questionsList.filter((question)=> question.id !== deletedQ.id)
  setQuestionList(updatedQuestionList)
 }
-  
+
+
+function handleUpdateQ(id, correctIndex){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        correctIndex
+      }),
+    })
+      .then((r) => r.json())
+      .then((updatedQ) => {
+  const updatedQuestionList = questionsList.map((question)=>{
+    if(question.id === updatedQ.id){
+      return updatedQ;
+    } else {
+      return question
+    }
+  })
+setQuestionList(updatedQuestionList)
+})
+}
 
   return (
     <section>
       <h1>Quiz Questions</h1>
       <ul>
         {questionsList.map((q)=>{
+            console.log(q.answers)
        return  <QuestionItem 
        question={q} 
        id={q.id} prompt={q.prompt} 
        answers={q.answers} 
        correctIndex={q.correctIndex} 
-       onDeleteClick={handleDeleteQuestion}/>
+       onDeleteClick={handleDeleteQuestion}
+       onUpdateQ={handleUpdateQ} 
+      />
         })}
       </ul>
     </section>
